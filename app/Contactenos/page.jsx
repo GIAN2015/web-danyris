7e0e235ec76e10c8775baaa5629ca51250df86ec
Gian Sinarahua <gian.sinarahua@danyris.com>
2025-06-09 11:29:41 -0500

    Fix: force dynamic rendering to avoid document is not defined in Contactenos

--- app/Contactenos/page.jsx
+++ app/Contactenos/page.jsx
@@ -1,14 +1,15 @@
 'use client';
+
+export const dynamic = 'force-dynamic';
+
 import dynamic from 'next/dynamic';
-
-
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { useRef, useState } from 'react'; 
 import emailjs from 'emailjs-com';
 
 const ReCAPTCHA = dynamic(() => import("react-google-recaptcha"), { ssr: false });
+
 
 
 export default function Contactenos() {
