36e801ddd490e3788d66c95eaeb62ddc3e83b50e
Gian Sinarahua <gian.sinarahua@danyris.com>
2025-07-17 09:08:52 -0500

    last commit

--- app/WhatsAppButton.jsx
+++ app/WhatsAppButton.jsx
@@ -2,7 +2,7 @@
 
 export default function WhatsAppButton() {
   const mensaje = encodeURIComponent("Hola, estoy interesado en sus servicios. ¿Podría brindarme más información? vengo de la página web de Danyris. Gracias. https://danyris.com/");
-  const numero = "01 741-0619";
+  const numero = "017410619";
 
   return (
     <a
